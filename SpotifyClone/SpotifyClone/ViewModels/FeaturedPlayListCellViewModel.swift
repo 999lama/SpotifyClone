55101cc8bef71ac0d01847c3c5f766abff42f06a
Lama Albadri <71217614+999lama@users.noreply.github.com>
2022-07-03 16:43:30 +0300

    set UI element for cells in browse

---
+++ SpotifyClone/SpotifyClone/ViewModels/FeaturedPlayListCellViewModel.swift
@@ -0,0 +1,14 @@
+//
+//  FeaturedPlayListCellViewModel.swift
+//  SpotifyClone
+//
+//  Created by Lama Albadri on 03/07/2022.
+//
+
+import Foundation
+
+struct FeaturedPlayListCellViewModel {
+    let name: String
+    let artWorkUrl: URL?
+    let creatorName: String?
+}
---
+++ SpotifyClone/SpotifyClone/ViewModels/RecommendedTrackCellViewModel.swift
@@ -0,0 +1,15 @@
+//
+//  RecommendedTrackCellViewModel.swift
+//  SpotifyClone
+//
+//  Created by Lama Albadri on 03/07/2022.
+//
+
+import Foundation
+
+struct RecommendedTrackCellViewModel {
+    let name: String
+    let artistName: String
+    let artWorkUrl: URL?
+    
+}
