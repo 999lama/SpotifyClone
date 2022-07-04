4b5f557b3dbfb2ce725442079bc2b53028285a8b
Lama Albadri <71217614+999lama@users.noreply.github.com>
2022-07-04 13:22:30 +0300

    Add header view for playList Screen

---
+++ SpotifyClone/SpotifyClone/ViewModels/PlayListHeaderViewModel.swift
@@ -0,0 +1,16 @@
+//
+//  PlayListHeaderViewModel.swift
+//  SpotifyClone
+//
+//  Created by Lama Albadri on 04/07/2022.
+//
+
+import Foundation
+
+struct PlayListHeaderViewModel {
+    let name: String?
+    let ownerName: String?
+    let description: String?
+    let artWorkURL: URL?
+    
+}
