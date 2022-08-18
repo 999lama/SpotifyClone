9b34bf73d0e42ed33496f174760bb836c1db3a07
Lama Albadri <71217614+999lama@users.noreply.github.com>
2022-08-18 11:18:48 +0300

    .

--- SpotifyClone/SpotifyClone/Controllers/Core/SearchViewController.swift
+++ SpotifyClone/SpotifyClone/Controllers/Core/SearchViewController.swift
@@ -7,17 +7,37 @@
 
 import UIKit
 
-class SearchViewController: UIViewController {
+class SearchViewController: UIViewController, UISearchResultsUpdating {
+
+    
 
     //MARK: - UI Elments
-    //TODO: Add UI Elemnts here
+    let searchController: UISearchController = {
+        let vc = UISearchController(searchResultsController: SearchResultsViewController())
+        vc.searchBar.searchBarStyle = .minimal
+        vc.definesPresentationContext = true
+        return vc
+    }()
     
     
     //MARK: - Life Cycle Methods
     override func viewDidLoad() {
         super.viewDidLoad()
         self.view.backgroundColor = .systemBackground
+        self.navigationItem.searchController = searchController
+        searchController.searchResultsUpdater = self
     }
     
+    func updateSearchResults(for searchController: UISearchController) {
+        guard let resultController = searchController.searchResultsController as? SearchResultsViewController,
+              let query = searchController.searchBar.text,
+              !query.trimmingCharacters(in: .whitespaces).isEmpty else {
+            return
+        }
+        
+        print(query)
+        // pefrom search
+        // APICaller.Shared.search
+    }
 
 }
